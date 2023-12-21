9ef92f76a8fd26455b809a86d04e39239f520e38
rotoyouoi <rotoyouoi@yandex.ru>
2023-12-21 09:27:50 +0500

    Добавьте файлы проекта.

---
+++ bebraProject/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bebraProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] existingArray = { -2, -4, 0, 1, 2, 3, 4, 5 };
+            List<int> numbersList = new List<int>();
+
+            for (int i = 0; i < existingArray.Length; i++)
+            {
+
+                numbersList.Add(existingArray[i]); 
+                
+            }
+
+
+
+            
+
+
+
+
+            foreach (int number in numbersList)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
